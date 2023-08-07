c874ce3bfe0767e9a9729a021a09f8b64010c0a0
Wes <108998223+wesgunning@users.noreply.github.com>
2023-08-07 13:56:10 -0700

    Change header color styling. Add CSS for book cards

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-let myLibrary = [];
-
+let myLibrary = [
+    {
+        title: 'The Hobbit',
+        author: 'J.R.R. Tolkien',
+        pages: '287',
+        readStatus: 'yes'
+    },
+    {
+        title: "Harry Potter & the Sorcerer's Stone",
+        author: 'J.K. Rowling',
+        pages: '307',
+        readStatus: 'yes'
+        },
+    {
+        title: 'The Martian',
+        author: 'Andy Weir',
+        pages: '305',
+        readStatus: 'yes'
+        }
+];
 function Book(title, author, pages, readStatus) {
-
+    this.title = title,
+    this.author = author,
+    this.pages = pages,
+    this.readStatus = readStatus;
 }
 
 function addBookToLibrary() {
     
+}
+
+function openForm() {
+    let form = document.createElement('form');
+    document.body.appendChild(form);
 }
